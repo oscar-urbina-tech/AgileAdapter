a149bad111ffd37ac129e5289bf3a3b5f577ab01
Oscar Urbina <oscar.urbina@dinkbit.com>
2016-02-17 16:59:46 -0600

    Generics added to populator.

--- src/main/java/com/agileandroid/ui/adapters/recycler/populator/Populator.java
+++ src/main/java/com/agileandroid/ui/adapters/recycler/populator/Populator.java
@@ -1,14 +1,20 @@
 package com.agileandroid.ui.adapters.recycler.populator;
 
 import com.agileandroid.ui.adapters.recycler.builder.AgileAdapterBuilder;
+import com.agileandroid.ui.adapters.recycler.holder.HolderRecycler;
 
 /**
  * Created by oscar.urbina on 8/28/15.
+ *
+ * @param <T> the type parameter
+ * @param <H> the type parameter
  */
-public abstract class Populator{
+public abstract class Populator<T, H extends HolderRecycler>{
 
     /**
      * Populate current view.
+     *
+     * @param agileAdapterBuilder the agile adapter builder
      */
-    public abstract void populate(final AgileAdapterBuilder agileAdapterBuilder);
+    public abstract void populate(final AgileAdapterBuilder<T, H> agileAdapterBuilder);
 }
