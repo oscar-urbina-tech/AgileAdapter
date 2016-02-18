f8cc4a5450e85bf30f8d7568f953b316933a3831
Oscar Urbina <oscar.urbina@dinkbit.com>
2016-02-18 12:39:57 -0600

    Generics added to Interactor

--- src/main/java/com/agileandroid/ui/adapters/view/interactor/Interactor.java
+++ src/main/java/com/agileandroid/ui/adapters/view/interactor/Interactor.java
@@ -1,14 +1,20 @@
 package com.agileandroid.ui.adapters.view.interactor;
 
 import com.agileandroid.ui.adapters.recycler.builder.AgileAdapterBuilder;
+import com.agileandroid.ui.adapters.recycler.holder.HolderRecycler;
 
 /**
  * Created by oscar.urbina on 8/28/15.
+ *
+ * @param <T> the type parameter
+ * @param <H> the type parameter
  */
-public abstract class Interactor {
+public abstract class Interactor<T, H extends HolderRecycler> {
 
     /**
      * Sets interaction.
+     *
+     * @param agileAdapterBuilder the agile adapter builder
      */
-    public abstract void setInteraction(AgileAdapterBuilder agileAdapterBuilder);
+    public abstract void setInteraction(AgileAdapterBuilder<T, H> agileAdapterBuilder);
 }
